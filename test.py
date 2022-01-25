4281ac24b11def9c433596c1a4ac2fa5268d7498
mrandronov <mrandronov88@gmail.com>
2022-01-25 10:37:52 -0500

    Revert "trying things out for a little while"
    This reverts commit 8067ba4209e1f6b3e337506faabf04c14bce7c4a.

--- test.py
+++ test.py
@@ -1,14 +1,11 @@
 
 
-<<<<<<< HEAD
 def add( x, y ):
     return x + y
 
 def subtract( x, y ):
     return x - y
 
-=======
->>>>>>> parent of f18c888 (Add function)
 def helloworld():
     print( "Hello World!" )
 
