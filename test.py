89ac35cc8d119193b6495c537ea89261b640d5a5
mrandronov <mrandronov88@gmail.com>
2022-01-25 10:36:26 -0500

    trying things out for a little while

--- test.py
+++ test.py
@@ -1,11 +1,14 @@
 
 
+<<<<<<< HEAD
 def add( x, y ):
     return x + y
 
 def subtract( x, y ):
     return x - y
 
+=======
+>>>>>>> parent of f18c888 (Add function)
 def helloworld():
     print( "Hello World!" )
 
